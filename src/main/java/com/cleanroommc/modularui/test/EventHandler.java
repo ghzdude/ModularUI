d9d23974bcfa720516376737046ae31f747338e0
brachy84 <brachy84@gmail.com>
2023-05-28 18:23:20 +0200

    replace scissor with stencil masks

--- src/main/java/com/cleanroommc/modularui/test/EventHandler.java
+++ src/main/java/com/cleanroommc/modularui/test/EventHandler.java
@@ -1,7 +1,5 @@
 package com.cleanroommc.modularui.test;
 
-import com.cleanroommc.modularui.factory.ClientGUI;
-
 import net.minecraft.init.Items;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,11 +10,10 @@ public class EventHandler {
     public static void onItemUse(PlayerInteractEvent.RightClickItem event) {
         if (event.getEntityPlayer().getEntityWorld().isRemote && event.getItemStack().getItem() == Items.DIAMOND) {
             //GuiManager.openClientUI(Minecraft.getMinecraft().player, new TestGui());
-            /*HoloUI.builder()
-                    .inFrontOf(Minecraft.getMinecraft().player, 5, false)
-                    .screenScale(0.5f)
-                    .open(new TestGui());*/
-            ClientGUI.open(new TestGui());
+            ModularScreenEntity modularScreenEntity = new ModularScreenEntity(event.getEntityPlayer().world);
+            modularScreenEntity.setScreen(new TestGui());
+            modularScreenEntity.setPosition(event.getEntity().posX, event.getEntity().posY, event.getEntity().posZ);
+            modularScreenEntity.spawnInWorld();
         }
     }
 }
