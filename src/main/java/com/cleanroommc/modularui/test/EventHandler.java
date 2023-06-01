cd912a482e023406ee057d4d2d5d4936f7e59209
brachy84 <brachy84@gmail.com>
2023-06-01 18:18:21 +0200

    holo ui's can face any direction... somewhat

--- src/main/java/com/cleanroommc/modularui/test/EventHandler.java
+++ src/main/java/com/cleanroommc/modularui/test/EventHandler.java
@@ -1,5 +1,8 @@
 package com.cleanroommc.modularui.test;
 
+import com.cleanroommc.modularui.holoui.HoloScreenEntity;
+import com.cleanroommc.modularui.holoui.HoloUI;
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Items;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -10,10 +13,9 @@ public class EventHandler {
     public static void onItemUse(PlayerInteractEvent.RightClickItem event) {
         if (event.getEntityPlayer().getEntityWorld().isRemote && event.getItemStack().getItem() == Items.DIAMOND) {
             //GuiManager.openClientUI(Minecraft.getMinecraft().player, new TestGui());
-            ModularScreenEntity modularScreenEntity = new ModularScreenEntity(event.getEntityPlayer().world);
-            modularScreenEntity.setScreen(new TestGui());
-            modularScreenEntity.setPosition(event.getEntity().posX, event.getEntity().posY, event.getEntity().posZ);
-            modularScreenEntity.spawnInWorld();
+            HoloUI.builder()
+                    .inFrontOf(Minecraft.getMinecraft().player, 5, false)
+                    .open(new TestGui());
         }
     }
 }
