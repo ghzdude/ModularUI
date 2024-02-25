70544bb4134c224bc272a4e536b1d62f8498b786
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-02-24 18:42:17 -0700

    vectors pls

--- src/main/java/com/cleanroommc/modularui/holoui/ScreenEntityRender.java
+++ src/main/java/com/cleanroommc/modularui/holoui/ScreenEntityRender.java
@@ -47,41 +47,46 @@ public class ScreenEntityRender extends Render<HoloScreenEntity> {
         } else {
             plane3D.transform();
         }
-        var mouse = calculateMousePos(player.getPositionVector(), entity, player.getLookVec());
+        var mouse = calculateMousePos(player, entity, player.getLookVec());
         screenWrapper.drawScreen(mouse.getX(), mouse.getY(), partialTicks);
         GlStateManager.popMatrix();
     }
 
-    private Vec3i calculateMousePos(Vec3d pos, HoloScreenEntity entity, Vec3d looking) {
+    private Vec3i calculateMousePos(EntityPlayer player, HoloScreenEntity entity, Vec3d looking) {
         var entpos = entity.getPositionVector();
+        var pos = player.getPositionVector();
+
         var plane = entity.getPlane3D();
         var planeRot = plane.getRotation();
         var diff = entpos.subtract(pos);
 
+        double l1 = Math.sqrt(looking.x * looking.x + looking.y * looking.y);
+        double l2 = Math.sqrt(looking.z * looking.z + looking.y * looking.y);
+        double x = Math.acos(looking.x / l1);
+        double y = Math.asin(looking.z / l2);
+
+        ModularUI.LOGGER.warn(String.format("looking x {%s}; y {%s}", Math.toDegrees(x), Math.toDegrees(y)));
         // horizontals only
-        var diffH = new Vec3d(diff.x, 0, diff.z);
-        var normalH = diffH.normalize();
-        var lookingH = new Vec3d(looking.x, 0, looking.z);
+//        var diffH = new Vec3d(diff.x, 0, diff.z);
+//        var normalH = diffH.normalize();
+//        var lookingH = new Vec3d(looking.x, 0, looking.z);
 
         // calculate offset to left/right of screen
-        double offsetH = Math.atan((plane.getWidth() / 16 / 2) / diff.length());
-        var lookRel = lookingH.subtract(normalH);
-        double lookRH = Math.atan(lookRel.z / lookRel.x);
+//        double offsetH = Math.atan((plane.getWidth() / 16 / 2) / diffH.length());
+//        var lookRel = lookingH.subtract(normalH).normalize();
+//        double lookRH = Math.atan(lookRel.z / lookRel.x) - (Math.PI / 4);
 
         // check if looking vector is within screen
-        if (lookRH < offsetH && lookRH > -offsetH) {
-            // diff is the vector from pos to entity
-            // i need to check if the look vector is within the bounds of the screen
-            // which means i need to rotate diff or something to line up with the edges of the plane
-            // i would be adding width to z assuming the plane is to the right from 0, 0
-            // height would be added to y as well
-            // however the plane could be at any point, so a rotation would be nicer
-            // i only need the rotation from the above example, which i could use
-            // i need the length of diff over the width of the plane
-            // this /should/ be looking at the plane
-            // now to actually calculate the mouse pos
-            ModularUI.LOGGER.warn("looking at screen maybe");
-        }
+        // diff is the vector from pos to entity
+        // i need to check if the look vector is within the bounds of the screen
+        // which means i need to rotate diff or something to line up with the edges of the plane
+        // i would be adding width to z assuming the plane is to the right from 0, 0
+        // height would be added to y as well
+        // however the plane could be at any point, so a rotation would be nicer
+        // i only need the rotation from the above example, which i could use
+        // i need the length of diff over the width of the plane
+        // this /should/ be looking at the plane
+        // now to actually calculate the mouse pos
         return new Vec3i(0, 0, 0);
     }
 }
